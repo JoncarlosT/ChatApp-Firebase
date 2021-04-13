41c2a4cdcff69b7801b6fc94b0377e860952b12b
JoncarlosT <joncarlos.tavarez@gmail.com>
2021-04-13 16:35:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
 
-function App() {
+import GlobalStyle from "./GlobalStyles";
+import { DarkTheme, LightTheme } from "./GlobalStyles";
+
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBsN7j9R_2lzH1r8FQhKeAyJpUfGd0KHSo",
+  authDomain: "chatapp-bcaba.firebaseapp.com",
+  projectId: "chatapp-bcaba",
+  storageBucket: "chatapp-bcaba.appspot.com",
+  messagingSenderId: "837688916956",
+  appId: "1:837688916956:web:160b61c6d654d8758eed19",
+  measurementId: "G-H6QZ3M995J",
+});
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+// const analytics = firebase.analytics();
+
+export default function App() {
+  //DARK MODE
+  const [darkMode, setDarkMode] = useState(false);
+
+  const changeTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  //USER AUTH
+  const [user] = useAuthState(firebase.auth());
+  console.log(user);
+
+  const signInWithGoogle = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().useDeviceLanguage();
+
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? DarkTheme : LightTheme}>
+      <StyledApp>
+        {user ? (
+          <StyledButton onClick={signOut}>SignOut</StyledButton>
+        ) : (
+          <StyledButton onClick={signInWithGoogle}>SignIn</StyledButton>
+        )}
+
+        <StyledThemeButton onClick={changeTheme}>
+          <Brightness4Icon style={{ fontSize: 50 }} />
+        </StyledThemeButton>
+        <GlobalStyle />
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
-export default App;
+const StyledApp = styled.div`
+  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.fontColor};
+  height: 100vh;
+`;
+
+const StyledButton = styled.button`
+  background-image: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.fontColor};
+  border: none;
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 10px;
+`;
+
+const StyledThemeButton = styled.button`
+  background-image: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.fontColor};
+  border: none;
+  border-radius: 100%;
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 10px;
+  align-items: center;
+  display: flex;
+`;
---
+++ src/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0
+    }
+`;
+
+export const DarkTheme = {
+  body: "#0a012e",
+  fontColor: "#fff",
+  primary: "linear-gradient(to right, rgb(237, 33, 58), rgb(147, 41, 30))",
+};
+
+export const LightTheme = {
+  body: "#f2f0fa",
+  fontColor: "#000",
+  primary: "linear-gradient(to right, rgb(0, 180, 219), rgb(0, 131, 176))",
+};
+
+export default GlobalStyle;
